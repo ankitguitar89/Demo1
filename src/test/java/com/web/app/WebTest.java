88af573c2ed0af5b10d9eb4be4451368e4561ba5
ankitguitar89 <ankitguitar89@gmail.com>
2017-09-22 16:19:33 +0530

    New File added.

--- src/test/java/com/web/app/WebTest.java
+++ src/test/java/com/web/app/WebTest.java
@@ -15,9 +15,10 @@ public class WebTest {
 	public static void LaunchSite(String URL) {
 		System.setProperty("webdriver.gecko.driver", ".\\Drivers\\FirefoxDriver\\geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
+		String a ="This is a string";
 		driver.get(URL);
 		ArrayList<String> a = new ArrayList<String>();
 		System.out.println("Title of the page is ="+driver.getTitle());
-		//System.out.println("Page Source of the page is ="+driver.getPageSource());
+		System.out.println("Page Source of the page is ="+driver.getPageSource());
 	}
 }
